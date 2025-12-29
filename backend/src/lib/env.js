43d70e15d5d984081b403f6795c1fcd4a611c45e
SwaroopDangal <swaroopdangal732@gmail.com>
2025-12-29 17:19:59 +0545

    first commit

---
+++ backend/src/lib/env.js
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+
+dotenv.config({ quiet: true });
+
+export const ENV = {
+  DB_URL: process.env.DB_URL,
+  PORT: process.env.PORT,
+  NODE_ENV: process.env.NODE_ENV,
+};
---
+++ backend/src/server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import { ENV } from "./lib/env.js";
+import path from "path";
+import { connectDB } from "./lib/db.js";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./lib/inngest.js";
+import { clerkMiddleware } from "@clerk/express";
+
+import cors from "cors";
+
+const app = express();
+
+const __dirname = path.resolve();
+
+//middleware
+app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); //this adds req.auth
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => {
+      console.log(`Server is running on port ${ENV.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting server", error);
+  }
+};
+
+startServer();
