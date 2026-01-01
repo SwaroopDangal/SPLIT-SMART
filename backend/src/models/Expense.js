8b33348d5f9ffba227e45c6d02935068dbfc83e3
SwaroopDangal <swaroopdangal732@gmail.com>
2026-01-01 09:02:27 +0545

    created expense model

---
+++ backend/src/models/Expense.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const expenseSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    paidBy: [
+      {
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        amount: {
+          type: Number,
+          required: true,
+          min: 0,
+        },
+      },
+    ],
+    splitAmong: [
+      {
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        amount: {
+          type: Number,
+          required: true,
+          min: 0,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Expense = mongoose.model("Expense", expenseSchema);
+
+export default Expense;
